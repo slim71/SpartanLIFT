078cbfe0813268fc52cdd4d673598ecfb8db0bcc
Simone Vollaro <slim71sv@gmail.com>
2023-06-30 22:58:42 +0200

    Raft: first functions for agent handling

--- src/pelican/include/pelican/pelican.hpp
+++ src/pelican/include/pelican/pelican.hpp
@@ -1,10 +1,11 @@
-#ifndef PELICAN_HPP_
-#define PELICAN_HPP_
+#ifndef _PELICAN_HPP_
+#define _PELICAN_HPP_
 
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/vehicle_local_position.hpp>
+#include "comms/msg/datapad.hpp"
 
-enum possible_roles { candidate, follower, leader };
+enum possible_roles { tbd, candidate, follower, leader };
 
 
 class PelicanUnit : public rclcpp::Node {
@@ -22,9 +23,17 @@ class PelicanUnit : public rclcpp::Node {
     private: 
         void parseModel();
         
-        void leaderSelection(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg) const;
+        void leaderSelection(const comms::msg::Datapad::SharedPtr msg) const;
 
         void printData(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg) const;
+
+        void becomeLeader();
+
+        void becomeFollower();
+
+        void becomeCandidate();
+
+        void vote(int id_to_vote);
 
     // Attributes
     private:
@@ -34,14 +43,19 @@ class PelicanUnit : public rclcpp::Node {
         std::string model_;
         double mass_ {0.0};
         
-        possible_roles role_ { candidate };
+        possible_roles role_ { tbd };
         int leader_id_ {0};
         
-        std::string leader_topic_ { "/fleet/leader_selection" };
-        rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr sub_to_leader_topic_;
+        std::string leader_selection_topic_ { "/fleet/leader_selection" };
+        rclcpp::Subscription<comms::msg::Datapad>::SharedPtr sub_to_leader_selection_topic_;
+        rclcpp::Publisher<comms::msg::Datapad>::SharedPtr pub_to_leader_selection_topic_;
 
         std::string local_pos_topic_ { "/fmu/out/vehicle_local_position" };
-        rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr sub_to_local_pos_;
+        rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr sub_to_local_pos_topic_;
+
+        bool leader_elected_ {false};
+
+        int current_term_ {0};
 };
 
 #endif
