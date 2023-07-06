0ee5d48be42e2860bfd76289cae4a5e6a99fa44a
Simone Vollaro <slim71sv@gmail.com>
2023-07-06 21:40:49 +0200

    Heartbeat handling

--- src/pelican/include/pelican/pelican.hpp
+++ src/pelican/include/pelican/pelican.hpp
@@ -3,10 +3,17 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <queue>
 #include "comms/msg/datapad.hpp"
+#include "comms/msg/heartbeat.hpp"
 
 enum possible_roles { tbd, candidate, follower, leader };
 
+struct heartbeat {
+    int term;
+    int leader;
+    rclcpp::Time timestamp;
+};
 
 class PelicanUnit : public rclcpp::Node {
     public:
@@ -18,6 +25,9 @@ class PelicanUnit : public rclcpp::Node {
         std::string get_model();
         double get_mass();
         possible_roles get_role();
+        int get_current_term();
+
+        bool isLeader();
 
     // Member functions
     private: 
@@ -34,6 +44,18 @@ class PelicanUnit : public rclcpp::Node {
         void becomeCandidate();
 
         void vote(int id_to_vote);
+
+        void randomTimer();
+
+        void sendHeartbeat();
+
+        void requestVote();
+
+        void stopHeartbeat();
+
+        void checkHeartbeat();
+
+        void storeHeartbeat(const comms::msg::Heartbeat msg);
 
     // Attributes
     private:
@@ -53,9 +75,29 @@ class PelicanUnit : public rclcpp::Node {
         std::string local_pos_topic_ { "/fmu/out/vehicle_local_position" };
         rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr sub_to_local_pos_topic_;
 
+        std::string heartbeat_topic_ { "/fleet/heartbeat" };
+        rclcpp::Subscription<comms::msg::Heartbeat>::SharedPtr sub_to_heartbeat_topic_;
+        rclcpp::Publisher<comms::msg::Heartbeat>::SharedPtr pub_to_heartbeat_topic_;
+
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::TimerBase::SharedPtr hb_monitoring_timer_;
+        rclcpp::TimerBase::SharedPtr hb_transmission_timer_;
+        bool hb_monitoring_running_ {false};
+        std::thread heartbeat_thread_ {};
+
         bool leader_elected_ {false};
 
         int current_term_ {0};
+
+        std::queue<heartbeat> received_hbs_;
+
+        // rclcpp::Clock system_clock;
+
+        std::mutex hbs_mutex_; // to use with received_hbs
+
+        rclcpp::CallbackGroup::SharedPtr exclusive_group_;
+        // Used to allow storeHeartbeat() and checkHeartbeat() to run in parallel
+        rclcpp::CallbackGroup::SharedPtr reentrant_group_;
 };
 
 #endif
