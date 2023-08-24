4916108ffbf684a9704421468ade00761fcc92d6
Simone Vollaro <slim71sv@gmail.com>
2023-08-24 12:50:47 +0200

    Renamed files

--- src/pelican/test/behavior_test.cpp
+++ src/pelican/test/behavior_test.cpp
@@ -3,6 +3,7 @@
 TEST_F(PelicanTest, TestRoleCoherence) {
     // Wait just to be sure the node is up and running completely
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    // this is a XOR statement: only one must be true
     ASSERT_TRUE((this->node.get()->isLeader() != this->node.get()->isFollower()) != this->node.get()->isCandidate());
 
 }
