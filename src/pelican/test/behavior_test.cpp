b95bf343edb3635a47a053642be5899b2fc94f6e
Simone Vollaro <slim71sv@gmail.com>
2023-08-25 12:52:32 +0200

    Some more behavioral tests

--- src/pelican/test/behavior_test.cpp
+++ src/pelican/test/behavior_test.cpp
@@ -7,3 +7,68 @@ TEST_F(PelicanTest, TestRoleCoherence) {
     ASSERT_TRUE((this->node.get()->isLeader() != this->node.get()->isFollower()) != this->node.get()->isCandidate());
 
 }
+
+// TODO: not working for some reason; data not sent if no simulation is run?
+// TEST_F(PelicanTest, TestPositionPublisher) {
+//     this->PositionPublisherTester();
+//
+//     for(int i=0; i < 10; i++) {
+//         this->data_ok_mutex.lock();
+//         bool a = this->data_ok;
+//         this->data_ok_mutex.unlock();
+//
+//         EXPECT_TRUE(a);
+//         if(a) break;
+//
+//         std::this_thread::sleep_for(std::chrono::seconds(1));
+//     }
+//
+// }
+
+TEST_F(PelicanTest, TestHeartbeatPublisher) {
+    this->HeartbeatPublisherTester();
+
+    while(!this->node->isLeader()) {
+
+    }
+
+    bool passed = false;
+    for(int i=0; i < 10; i++) {
+        this->data_ok_mutex.lock();
+        bool a = this->data_ok;
+        this->data_ok_mutex.unlock();
+
+        if(a) {
+            SUCCEED();
+            passed = true;
+            break;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    if(!passed) FAIL();
+
+}
+
+TEST_F(PelicanTest, TestDatapadPublisher) {
+    this->DatapadPublisherTester();
+
+    bool passed = false;
+    for(int i=0; i < 10; i++) {
+        this->data_ok_mutex.lock();
+        bool a = this->data_ok;
+        this->data_ok_mutex.unlock();
+
+        if(a) {
+            SUCCEED();
+            passed = true;
+            break;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    if(!passed) FAIL();
+
+}
--- src/pelican/test/fixtures.cpp
+++ src/pelican/test/fixtures.cpp
@@ -3,7 +3,6 @@
 void PelicanTest::SetUp() {
     std::string pelican_share_directory = ament_index_cpp::get_package_share_directory("pelican");
     std::string config_file_path = pelican_share_directory + "/config/copter_test.yaml";
-    std::cout << "config_file_path=" << config_file_path << std::endl;
 
     char* argv[] = {
         strdup("--ros-args"),
@@ -15,6 +14,7 @@ void PelicanTest::SetUp() {
 
     // Initialization
     rclcpp::init(argc, argv);
+
     // Clean up dynamically allocated memory
     for (int i = 0; i < argc; ++i) {
         free(argv[i]);
@@ -25,14 +25,50 @@ void PelicanTest::SetUp() {
     // Set the instance pointer to the shared pointer of the main node
     Pelican::setInstance(this->node);
 
+    this->reentrant_group_ = this->node->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+    this->reentrant_opt_.callback_group = this->reentrant_group_;
+
     this->executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
     this->executor->add_node(this->node);
 
     this->spin_thread = std::thread([this](){this->executor->spin();});
-};
+}
 
 void PelicanTest::TearDown() {
     this->executor->cancel();
     this->spin_thread.join(); // Wait for thread completion
     rclcpp::shutdown();
-};
+}
+
+void PelicanTest::PositionPublisherTester() {
+
+    this->pos_sub = this->node->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
+        "/fmu/out/vehicle_local_position", this->px4_qos_, 
+        [this](const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg) { 
+            std::lock_guard<std::mutex> lock(this->data_ok_mutex);
+            this->data_ok = true; 
+        },
+        this->reentrant_opt_);
+}
+
+void PelicanTest::HeartbeatPublisherTester() {
+
+    this->hb_sub = this->node->create_subscription<comms::msg::Heartbeat>(
+        "/fleet/heartbeat", this->standard_qos_, 
+        [this](const comms::msg::Heartbeat::SharedPtr msg) { 
+            std::lock_guard<std::mutex> lock(this->data_ok_mutex);
+            this->data_ok = true; 
+        },
+        this->reentrant_opt_);
+}
+
+void PelicanTest::DatapadPublisherTester() {
+
+    this->data_sub = this->node->create_subscription<comms::msg::Datapad>(
+        "/fleet/leader_selection", this->standard_qos_, 
+        [this](const comms::msg::Datapad::SharedPtr msg) { 
+            std::lock_guard<std::mutex> lock(this->data_ok_mutex);
+            this->data_ok = true; 
+        },
+        this->reentrant_opt_);
+}
