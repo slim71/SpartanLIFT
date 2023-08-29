f98918bac8557926c88a1123a46f6768bf8af8f2
Simone Vollaro <slim71sv@gmail.com>
2023-08-29 22:52:29 +0200

    Utilities and cleaning

---
+++ src/pelican/include/pelican/utilities.hpp
@@ -0,0 +1,14 @@
+#ifndef _UTILITIES_HPP_
+#define _UTILITIES_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <iostream>
+#include "types.hpp"
+
+void resetTimer(rclcpp::TimerBase::SharedPtr&);
+void cancelTimer(rclcpp::TimerBase::SharedPtr&);
+template<typename T> void resetSharedPointer(std::shared_ptr<T>&);
+
+#include "templates.tpp"
+
+#endif
