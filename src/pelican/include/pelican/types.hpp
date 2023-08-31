30da266e93ee9242feeb6c4d5e44625e0e4ae551
Simone Vollaro <slim71sv@gmail.com>
2023-08-31 22:56:26 +0200

    Improved macros

--- src/pelican/include/pelican/types.hpp
+++ src/pelican/include/pelican/types.hpp
@@ -14,15 +14,33 @@ struct heartbeat {
         rclcpp::Time timestamp;
 };
 
-#define ROLES(F) \
-    F(tbd)       \
-    F(candidate) \
-    F(follower)  \
-    F(leader)
+/***************** Enum Macros / X-Macros ****************/
+// Macro "constructors" for type and string tables
+#define AS_BARE(a) a ,
+#define AS_STR(a) #a ,
 
-#define F(name) name,
-enum possible_roles { ROLES(F) NumPossibleRoles };
-#undef F
+/************************ Modules ************************/
+// Table; _ for any substitution
+#define MODULES(_) \
+    _(nullmodule)  \
+    _(main_module) \
+    _(log_module)  \
+    _(hb_module)
+
+enum possible_modules { MODULES(AS_BARE) NumModules };
+
+std::string modules_to_string(possible_modules);
+possible_modules string_to_modules(const std::string&);
+
+/************************* Roles *************************/
+// Table; _ for any substitution
+#define ROLES(_) \
+    _(tbd)       \
+    _(candidate) \
+    _(follower)  \
+    _(leader)
+
+enum possible_roles { ROLES(AS_BARE) NumPossibleRoles };
 
 std::string roles_to_string(possible_roles);
 possible_roles string_to_roles(const std::string&);
--- src/pelican/src/types.cpp
+++ src/pelican/src/types.cpp
@@ -1,8 +1,7 @@
 #include "types.hpp"
 
-#define F(name) #name,
-const char* roles_names[] = {ROLES(F) nullptr};
-#undef F
+const char* modules_names[] = { MODULES(AS_STR) nullptr };
+const char* roles_names[] = {ROLES(AS_STR) nullptr};
 
 std::string roles_to_string(possible_roles role) {
     return ((int) role < (int) NumPossibleRoles) ? roles_names[(int) role] : "";
@@ -18,3 +17,16 @@ possible_roles string_to_roles(const std::string& s) {
     return r;
 }
 
+std::string modules_to_string(possible_modules module) {
+    return ((int) module < (int) NumPossibleRoles) ? modules_names[(int) module] : "";
+}
+
+possible_modules string_to_modules(const std::string& s) {
+    possible_modules m = nullmodule;
+    auto result = std::find(modules_names, modules_names + NumModules, s);
+
+    if (result != modules_names + NumModules)
+        m = possible_modules(result - modules_names);
+
+    return m;
+}
