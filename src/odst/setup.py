b8002a77bf83e93c410c52eb5142d4e267cd4f50
Simone Vollaro <slim71sv@gmail.com>
2023-09-30 11:08:08 +0200

    Minor changes

--- src/odst/setup.py
+++ src/odst/setup.py
@@ -6,21 +6,23 @@ package_name = 'odst'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.0',  # TODO: increase
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include all config files
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Simone Vollaro',
     maintainer_email='slim71sv@gmail.com',
     description='Launch files package for the whole project',
-    license='TODO: License declaration',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
