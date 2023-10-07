e8af5bae73e525fa10ca6cbb2fc688458e5296e1
Simone Vollaro <slim71sv@gmail.com>
2023-10-07 16:06:49 +0200

    Example launch files

--- src/odst/odst/logs.py
+++ src/odst/odst/logs.py
@@ -1,6 +1,7 @@
 from launch.actions import LogInfo
 
-def print_argument(context):
+
+def print_arguments(context):
     stuff_to_log = []
 
     stuff_to_log.append(LogInfo(msg="=====Launch Configuration Values ====="))
@@ -9,3 +10,21 @@ def print_argument(context):
         stuff_to_log.append(LogInfo(msg=f'\'{var}\' has value \'{value}\''))
 
     return stuff_to_log
+
+
+class LogDebug():
+    """
+    Simple class made to be able to log something only if the 'debug' level is specified.
+    Preferred as workaround instead of using global variables/constants.
+    """
+    loglevel: str
+
+    def __init__(self):
+        self.loglevel = 'info'
+
+    def print(self, msg_content: str):
+        if self.loglevel == 'debug':
+            print(f'[DEBUG] {msg_content}')
+
+    def setLevel(self, l: str):
+        self.loglevel = l
