62c1cdec9cefb433082042023436587a9f0e440b
Simone Vollaro <slim71sv@gmail.com>
2024-01-01 13:15:21 +0100

    Adding ROS code to main launch file

--- src/odst/launch/fleet.launch.py
+++ src/odst/launch/fleet.launch.py
@@ -3,91 +3,115 @@ import sys
 import yaml
 from odst.logs import LogDebug  # Gives error in VSCode but it's valid
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
+    level = "info"
+    pkg = "odst"
+    launchfile = "ros_agents.launch.py"
+
     logger = LogDebug()
-
-    # Parse arguments manually to use them in this script too
-    for arg in sys.argv:
-        if arg.startswith('loglevel:='):
-            logger.setLevel(str(arg.split(':=')[1]))
-
     launch_description = LaunchDescription()
 
+    # Parse loglevel argument manually to use it in this script too
+    for arg in sys.argv:
+        if arg.startswith("loglevel:="):
+            logger.setLevel(str(arg.split(":=")[1]))
+            level = logger.getLevel()
+
     # Get the filepath to your config file
-    config_file = os.path.join(
-        get_package_share_directory('odst'),
-        'config',
-        'fleet.yaml'
-    )
+    config_file = os.path.join(get_package_share_directory(pkg), "config", "fleet.yaml")
     # Load the parameters specific to your ComposableNode
-    with open(config_file, 'r', encoding='utf8') as file:
-        config_params = yaml.safe_load(file)['launchfile']
+    with open(config_file, "r", encoding="utf8") as file:
+        config_params = yaml.safe_load(file)["launchfile"]
 
     # Extract the number of agents
-    agent_num = config_params['fleet_size']
+    agent_num = config_params["fleet_size"]
 
     # Script used when starting Gazebo
-    source_file = os.path.join(
-        get_package_share_directory('odst'),
-        'resource',
-        'include_px4_models.sh'
+    additional_source_file = os.path.join(
+        get_package_share_directory(pkg), "resource", "include_px4_models.sh"
     )
+
+    source_local_wos = "source install/setup.bash"
+
     # World to use in Gazebo
-    world_name = config_params['world']
-    logger.print(f'Using world {world_name}')
+    world_name = config_params["world"]
+    logger.print(f"Using world {world_name}")
 
     # Get the filepath to your config file
-    script_file = os.path.join(
-        get_package_share_directory('odst'),
-        'temp',
-        'terminal.sh'
-    )
-    # Write every gnome-terminal in a temp file and use that to spawn everything in the same gnome-terminal window
-    with open(script_file, 'w+', encoding='utf8') as tmp:
-        tmp.write('#!/bin/bash')  # Bash shebang
-        tmp.write('\n')
+    script_file = os.path.join(get_package_share_directory(pkg), "temp", "terminal.sh")
+    # Write every gnome-terminal in a temp file and use that to spawn
+    # everything in the same gnome-terminal window
+    with open(script_file, "w+", encoding="utf8") as tmp:
+        tmp.write("#!/bin/bash")  # Bash shebang
+        tmp.write("\n")
 
         # Add the MicroXRCE agent command
-        agent_cmd = 'gnome-terminal --tab -t \'MicroXRCE agent\' -- bash -c \'source install/setup.bash; MicroXRCEAgent udp4 -p 8888\''
+        agent_cmd = (
+            "gnome-terminal --tab -t 'MicroXRCE agent' "
+            f"-- bash -c '{source_local_wos}; MicroXRCEAgent udp4 -p 8888'"
+        )
         logger.print(agent_cmd)
         tmp.write(agent_cmd)
-        tmp.write('\n')
+        tmp.write("\n")
 
         # Source a custom script to include the PX4 model folder, then manually start Gazebo;
         # without this, Gazebo would not find all needed models.
-        # '-r' is needed to start the simulation right away, since PX4 checks for the Gazebo's clock values
-        gazebo_server_cmd = f'gnome-terminal --tab -t \'Gazebo\' -- bash -c \'source {source_file}; gz sim -r {world_name}.sdf\''
+        # '-r' is needed to start the simulation right away, since PX4
+        # checks for the Gazebo's clock values
+        gazebo_server_cmd = (
+            "gnome-terminal --tab -t 'Gazebo' "
+            f"-- bash -c 'source {additional_source_file}; gz sim -r {world_name}.sdf'"
+        )
         logger.print(gazebo_server_cmd)
         tmp.write(gazebo_server_cmd)
-        tmp.write('\n')
+        tmp.write("\n")
 
         # Prepare each PX4 instance
+        simulation_headstart = 10
         for count in range(agent_num):  # CHECK: change with enumerate?
-            code = config_params['codes'][count]
-            x = config_params['xs'][count]
-            y = config_params['ys'][count]
-            model = config_params['models'][count]
+            code = config_params["codes"][count]
+            x = config_params["xs"][count]
+            y = config_params["ys"][count]
+            model = config_params["models"][count]
 
             # Add the command to start the PX4 simulation for the model
-            px4_cmd = f'PX4_SYS_AUTOSTART={code} PX4_GZ_MODEL_POSE=\'{x},{y}\' PX4_GZ_MODEL={model} ./build/px4_sitl_default/bin/px4 -i {count+1}'
+            px4_cmd = (
+                f"PX4_SYS_AUTOSTART={code} PX4_GZ_MODEL_POSE='{x},{y}' "
+                f"PX4_GZ_MODEL={model} ./build/px4_sitl_default/bin/px4 -i {count+1}"
+            )
             # Add some wait time for subsequent spawns
             if count > 0:
-                px4_cmd = f'sleep {10+count}; {px4_cmd}'
-            px4_cmd = f'gnome-terminal --tab -t \'PX4 - {model}\' -- bash -c \'cd PX4-Autopilot; {px4_cmd}\''
+                simulation_headstart += count
+                px4_cmd = f"sleep {simulation_headstart}; {px4_cmd}"
+            px4_cmd = (
+                f"gnome-terminal --tab -t 'PX4 - {model}' "
+                f"-- bash -c 'cd PX4-Autopilot; {px4_cmd}'"
+            )
             logger.print(px4_cmd)
             tmp.write(px4_cmd)
-            tmp.write('\n')
+            tmp.write("\n")
+
+        # Manually execute launch file in order to get the logs in a standalone gnome terminal tab
+        # Give an additional 10s to be sure everything has started correctly
+        sleep_part = f"sleep {10 + simulation_headstart}"
+        agents_launchfile = f"ros2 launch {pkg} {launchfile} loglevel:={level}"
+        launch_cmd = (
+            "gnome-terminal --tab -t 'ROS2 nodes' "
+            f"-- bash -c ' {sleep_part}; {source_local_wos}; {agents_launchfile}; bash'"
+        )
+        logger.print(launch_cmd)
+        tmp.write(launch_cmd)
+        tmp.write("\n")
 
         # Interactive shell
-        shell_cmd = 'gnome-terminal --tab -t \'Interactive shell\' -- bash'
+        shell_cmd = "gnome-terminal --tab -t 'Interactive shell' -- bash"
         logger.print(shell_cmd)
         tmp.write(shell_cmd)
-        tmp.write('\n')
+        tmp.write("\n")
 
     # Ensure the right permissions are set, to be able to execute the script
     os.chmod(script_file, 0o777)
@@ -95,11 +119,11 @@ def generate_launch_description():
     # Actually initiate spawn of everything
     bash_script = ExecuteProcess(
         cmd=[
-            'gnome-terminal',
-            '--window',
-            '--maximize',
-            '--',
-            f'{script_file}',
+            "gnome-terminal",
+            "--window",
+            "--maximize",
+            "--",
+            f"{script_file}",
         ],
     )
 
